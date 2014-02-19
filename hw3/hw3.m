00c37499789390cb0ab29f8af0b9732ae29f6473
me <me@mint>
2014-02-18 20:14:38 -0800

    changes made 2-18

--- hw3/hw3.m
+++ hw3/hw3.m
@@ -2,15 +2,9 @@ close all;
 clear;
 clc;
 
-user=getenv('username');
-
 if (ispc == 0)
-	%filepath = strcat('/home/',user,'/Dropbox/')
 	filepath = '/home/me/Dropbox/'
 	image = strcat(filepath, 'Space_Shuttle_Columbia_launching.jpg')
-	filepath = strcat('/home/',user,'/Dropbox/');
-	image = strcat(filepath, 'Space_Shuttle_Columbia_launching.jpg');
-
 else
 	filepath='D:\home\Documents\git\machine_vision\hw3\';
     image = strcat(filepath, 'Space_Shuttle_Columbia_launching.jpg');
@@ -25,14 +19,12 @@ end
 %--------%
 % transform an image to gray level
 I = imread(image);
-
-I=rgb2gray(I);
-figure('name' )
-
 I = rgb2gray(I);
-figure('name', 'original image');
-title('original image');
+I = imresize(I, [600,712]);
+figure('name', 'Problem 1');
+subplot(3,2,1);
 imshow(I);
+title('original image');
 
 
 %--------%
@@ -40,17 +32,15 @@ imshow(I);
 %--------%
 % add gaussian noise
 noisy = imnoise(I, 'gaussian');
-figure('name', 'noizy image');
-title('noizy image');
+subplot(3,2,2);
 imshow(noisy);
+title('noizy image');
 
 %--------%
 % part 3 %
 %--------%
 % create 5x5 gauss mask
 mask = fspecial('gaussian', [5 5], 0.5)
-test = conv2(noisy,mask);
-imshow(test);
 
 %--------%
 % part 4 %
@@ -61,9 +51,9 @@ imshow(test);
 cd ../hw2/
 space = convolution(noisy, mask);
 cd ../hw3/
-figure('name', 'convolved (filtered) image');
-title('convolved (filtered) image');
+subplot(3,2,3);
 imshow(space);
+title('convolved (filtered) image')
 
 %--------%
 % part 5 %
@@ -71,9 +61,9 @@ imshow(space);
 % fourier transform the image
 
 fftd = fft2(noisy);
-figure('name', 'fouriered image');
-title('fouriered image');
+subplot(3,2,4);
 imshow(fftd);
+title('fouriered image');
 
 %--------%
 % part 6 %
@@ -84,11 +74,11 @@ Z = fspecial('gaussian', [600 712], 0.5);
 Z = fft2(Z);
 filtered = fftd.*Z;
 
-figure('name', 'freq filtered image');
-title('freq filtered image');
+subplot(3,2,5);
 imageD = ifft2(filtered);
 dmin = min(min(abs(imageD))); dmax = max(max(abs(imageD)));
 imshow( ( ifftshift(imageD)), [dmin dmax]),
+title('freq filtered image');
 
 
 
@@ -100,10 +90,106 @@ imshow( ( ifftshift(imageD)), [dmin dmax]),
 % part 1 %
 %--------%
 % Apply the DCT on an image of your choice.
-I = imread(image);
-I = rgb2gray(I);
-I = double(I);
+imDCT=im2double(I);
 imDCT=dct2(I);
-figure('name', 'dct')
-title('dct')
+figure('name', 'Good DCT choices')
+subplot(3,2,1);
 imshow(log(abs(imDCT)), []), colormap(jet), colorbar;
+title('dct of original image')
+
+%--------%
+% part 2 %
+%--------%
+% cut these images, store top left
+% imDCT2 = cropper(imDCT, .5, 'normal');
+% [M,N]=size(imDCT);
+% imDCT2 = imcrop(imDCT,[0,0, M/2, N/2]);
+% imDCT4 = imcrop(imDCT,[0,0, M/4, N/4]);
+% imDCT8 = imcrop(imDCT,[0,0, M/8, N/8]);
+% imDCT16 = imcrop(imDCT,[0,0, M/16, N/16]);
+% imDCTtest = imcrop(imDCT, [0,0,M*.9, N*.9]);
+
+imDCT2=cropper(imDCT, .5, 'normal');
+imDCT4=cropper(imDCT, .25, 'normal');
+imDCT8=cropper(imDCT, .125, 'normal');
+imDCT16=cropper(imDCT, .0625, 'normal');
+
+
+%--------%
+% part 3 %
+%--------%
+% display the images
+subplot(3,2,2);
+imshow(log(abs(imDCT2)),[]), colormap(jet), colorbar;
+title('half of dct info')
+
+subplot(3,2,3);
+imshow(log(abs(imDCT4)),[]), colormap(jet), colorbar;
+title('25% of dct info')
+
+subplot(3,2,4);
+imshow(log(abs(imDCT8)),[]), colormap(jet), colorbar;
+title('12.5% of dct info')
+
+subplot(3,2,5)
+imshow(log(abs(imDCT16)),[]), colormap(jet), colorbar;
+title('6.25% of dct info')
+
+
+%--------%
+% part 4 %
+%--------%
+% invert the dct and display
+
+figure('name', 'inverse DCTd image')
+[M,N]=size(imDCT);
+K = uint8(imresize(sqrt(idct2(imDCT2)), [M,N]));
+subplot(2,2,1)
+imshow(K, [0 255]);
+title('half of data')
+
+K = uint8(imresize(sqrt(idct2(imDCT4)), [M N]));
+subplot(2,2,2)
+imshow(K, []);
+title('quarter of data')
+
+K = uint8(imresize(sqrt(idct2(imDCT8)), [M N]));
+subplot(2,2,3)
+imshow(K, []);
+title('eighth of data')
+
+K = uint8(imresize(sqrt(idct2(imDCT16)), [M N]));
+subplot(2,2,4)
+imshow(K, [0 255]);
+title('sixteenth of data')
+
+%--------%
+% part 5 %
+%--------%
+% cut these images, store bottom left
+badDCT2=cropper(imDCT, (1/2), 'bad');
+badDCT4=cropper(imDCT, (1/4), 'bad');
+badDCT8=cropper(imDCT, (1/8), 'bad');
+badDCT16=cropper(imDCT, (1/16), 'bad');
+
+% %--------%
+% % part 6 %
+% %--------%
+% % display dcts of bad stuffs
+% figure('name', 'bad dct data');
+% subplot(2,2,1)
+% imshow(log(abs(badDCT2)),[]), colormap(jet), colorbar;
+% title('half of data')
+
+
+% subplot(2,2,2)
+% imshow(log(abs(badDCT4)),[]), colormap(jet), colorbar;
+% title('quarter of data')
+
+% subplot(2,2,3)
+% imshow(log(abs(badDCT8)),[]), colormap(jet), colorbar;
+% title('eighth of data')
+
+% subplot(2,2,4)
+% imshow(log(abs(badDCT16)),[]), colormap(jet), colorbar;
+% title('sixteenth of data')
