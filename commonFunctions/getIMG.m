9366dc37a8c4c24b8f98219e84425e5782580053
curtis <cmuntz@outlook.com>
2014-02-25 14:20:20 -0800

    hw4 completed, simplifications still needed

--- commonFunctions/getIMG.m
+++ commonFunctions/getIMG.m
@@ -1,11 +1,14 @@
 function [I] = getIMG(imageNAMEwithextension)
 % Find the file (different path on my linux vs Win installs)
 % requires extension
-if (ispc == 0)
-	filepath = '/home/me/Dropbox/Pictures/';
-	image = strcat(filepath, imageNAMEwithextension);
-else
+if (ispc == 1)
 	filepath='D:\home\Dropbox\Pictures\';
+	image = strcat(filepath, imageNAMEwithextension);
+elseif(ismac == 1)
+	   filepath = '/Users/me/Dropbox/Pictures/';
+	   image = strcat(filepath, imageNAMEwithextension);
+else
+	filepath = '~/me/Dropbox/Pictures/';
 	image = strcat(filepath, imageNAMEwithextension);
 end
 
--- hw4/hw4.m
+++ hw4/hw4.m
@@ -92,11 +92,11 @@ subplot(224)
 c=10;
 sobelX=double([-1 0 1; -c 0 c; -1 0 1]);
 sobelY=double([1 c 1; 0 0 0; -1 -c -1]);
-startS=tic
+startS=tic;
 h=convolution(I,sobelX);
 v=convolution(I,sobelY);
 C=(abs(h)+abs(v));
-sobeltime= toc(startS)
+sobeltime= toc(startS);
 imshow(C);
 title('c=10')
 
@@ -108,9 +108,9 @@ title('c=10')
 %%
 figure('name', 'laplacian mask')
 L=[0 1 0; 1 -4 1; 0 1 0];
-startL = tic
+startL = tic;
 laplacedI=convolution(I,L);
-laplacetime = toc(startL)
+laplacetime = toc(startL);
 imshow(im2uint8(laplacedI)), title('laplacian mask');
 rmpath ../commonFunctions
 
@@ -136,64 +136,91 @@ sprintf('laplacian approach: %s', laplacetime)
 addpath ../commonFunctions
 I2 = getIMG('Soyuz_TMA-19_spacecraft_departs_the_ISS.jpg');
 rmpath ../commonFunctions
-I2 = im2double(rgb2gray(imresize(I2,[400,400])));
+I2 = im2double(rgb2gray(imresize(I2,[401,401])));
 imshow(I2);
 
 figure('name','ffts of sobel masks');
 c=2;
 sobelX=double([-1 0 1; -c 0 c; -1 0 1]);
 sobelY=double([1 c 1; 0 0 0; -1 -c -1]);
-[M,N]=size(sobelX);
-[M1,N1]=size(I2);
-sobelX1 = fft(padarray(sobelX,[ceil((ceil(M1/2)-ceil(M/2))),ceil((ceil(N1/2)-(ceil(N)/2))])));
-sobelY1 = fft(padarray(sobelY,[ceil((ceil(M1/2)-ceil(M/2))),ceil((ceil(N1/2)-(ceil(N)/2))])));
-subplot(121), imshow(sobelX1), title('sobelX fft');
-subplot(122), imshow(sobelY1), title('sobelY fft');
+
+sobelX1 = padarray(sobelX,[199,199]);
+sobelFFTX=fft2(sobelX1);
+%work on making this more general..........
+sobelY1 = padarray(sobelY,[199,199]);
+sobelFFTY=fft2(sobelY1);
+
+subplot(121), imshow(sobelFFTX), title('sobelX fft');
+subplot(122), imshow(sobelFFTY), title('sobelY fft');
 
 %--------%
 % part 2 %
 %--------%
 % Perform filtering in the frequency domain using the horizontal Sobel mask
 %%
-figure('name', 'frequency filtering');
-horizF=I2.*sobelX1;
+figure('name', 'frequency horizontal filtering');
+horizF=fft2(I2).*sobelFFTX;
+horizS=ifft2(horizF);
+dmin = min(min(abs(horizS)));
+dmax = max(max(abs(horizS)));
 subplot(121), imshow(horizF), title('horizontal sobel freq');
-
+subplot(122), imshow(ifftshift(horizS),[dmin dmax]), title('filtered img');
 %--------%
 % part 3 %
 %--------%
 % Perform filtering in the frequency domain using the vertical Sobel mask
 %%
-vertF=I2.*sobelX1;
-subplot(122), imshow(vertF), title('vertical sobel freq');
-
+figure('name', 'frequency vertical filtering');
+vertF=fft2(I2).*sobelFFTY;
+vertS=ifft2(vertF);
+dmin = min(min(abs(vertS)));
+dmax = max(max(abs(vertS)));
+subplot(121), imshow(vertF), title('horizontal sobel freq');
+subplot(122), imshow(ifftshift(vertS),[dmin dmax]), title('filtered img');
 %--------%
 % part 4 %
 %--------%
 % Obtain the FFT of the Laplacian mask. You need to perform zero padding
 %%
-L1 = fft(padarray(L,));
+L1 = fft2(padarray(L,[199,199]));
 
 %--------%
 % part 5 %
 %--------%
 % Perform filtering in the frequency domain using Laplacian to detect the edges.
 %%
-lapF=I2.*L1;
-figure('name','laplacian frequency filtering'), imshow(lapF);
-
+figure('name','laplacian frequency filtering');
+lapF=fft2(I2).*L1;
+lapS=ifft2(lapF);
+dmin = min(min(abs(lapS)));
+dmax = max(max(abs(lapS)));
+imshow(ifftshift(lapS),[dmin dmax]), title('Laplacian filtered img freq');
 %--------%
 % part 6 %
 %--------%
 % Construct a 3 by 3 mask for the Laplacian of Gaussian (LoG) and then obtain 
 % its FFT. You can use fspecial for this question
 %%
+% addpath ../commonFunctions
+% gauss=gaussfilter(3, 1/2);
+% LoG=L*gauss;
+% LoGF=fft2(LoG);
+% rmpath ../commonFunctions
+LoG=fspecial('log',[401,401]);
+LoGF=fft2(LoG);
 
 %--------%
 % part 7 %
 %--------%
 % Perform filtering in the frequency domain using LoG to detect the edges
 %%
+figure('name','laplacian of gaussian freq filtering');
+I3=fft2(I2).*L1;
+lapS=ifft2(I3);
+dmin = min(min(abs(lapS)));
+dmax = max(max(abs(lapS)));
+imshow(ifftshift(lapS),[dmin dmax]);
+title('Laplacian of gaussian filtered img freq');
 
 %--------%
 % part 8 %
@@ -213,4 +240,11 @@ figure('name','laplacian frequency filtering'), imshow(lapF);
 % detector to an image of your choice. Pick an image with visible corners. 
 % Mark all corners on the image in a similar way to figure 2
 %%
-addpath ../commonFunctions
+figure('name','corner detection');
+C=corner(I2); %to find corner points
+imshow(I2);hold on, plot(C(:,1),C(:,2),'ro')
+title('corners detected')
+figure('name','corner detection2')
+C=corner(I); %to find corner points
+imshow(I);hold on, plot(C(:,1),C(:,2),'ro')
+title('corners detected 2');
