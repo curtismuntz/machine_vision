0298c2fea3a1fce25a572fc3d1f9ffebf353a33b
curtis <cmuntz@outlook.com>
2014-02-25 21:25:15 -0800

    some more updates to hw4

--- hw4/hw4.m
+++ hw4/hw4.m
@@ -1,66 +1,55 @@
+%% EEE 178 Homework #4
+% MATLAB code and figures
 clear all;
 clc;
 close all;
 addpath ../commonFunctions
 
+%image preprocessing!
 I = getIMG('edgePhoto.jpg');
 I = imresize(I,[400,400]);
 I = rgb2gray(I);
 I = im2double(I);
 imshow(I);
+title('Original Image');
 
-%---------------------------------------------%
-% problem 1                                   %
-%---------------------------------------------%
+%% Problem 1
+% An edge detection in the space domain.
 
-%--------%
-% part 1 %
-%--------%
-% Apply the horizontal sobel mask to the image. show the result.
-%%
-sobelX=double([-1 0 1; -2 0 2; -1 0 1])
-
-
+%% Parts 1 and 2
+% Apply the horizontal and vertical sobel mask to the image.
+% show the results.
 figure('name', 'sobel masks using custom convultion')
 subplot(121)
+
+sobelX=double([-1 0 1; -2 0 2; -1 0 1])
 horiz = convolution(I, sobelX);
 imshow(im2uint8(horiz));
 title('sobelX')
 
-
-%--------%
-% part 2 %
-%--------%
-% Apply the vertical sobel mask to the image. show the result.
-%%
-sobelY=double([1 2 1; 0 0 0; -1 -2 -1])
 subplot(122)
+sobelY=double([1 2 1; 0 0 0; -1 -2 -1])
 vert = convolution(I, sobelY);
 imshow(im2uint8(vert));
 title('sobelY')
 
 
-%--------%
-% part 3 %
-%--------%
-% use the magnitude to combine edges and show result
-%%
+%% Part 3
+% Use the magnitude to combine edges and show result
 figure('name','magnitude'); %pop pop
 magnitude = abs(horiz) + abs(vert);
 imshow(magnitude);
 title('approximation of magnitude');
 
-%--------%
-% part 4 %
-%--------%
-% Use different constant for c in sobel mask. try different values and see what happens
-%%
+
+%% Part 4
+% Use different constant for c in sobel mask. try different
+% values and see what happens.
 figure('name', 'various values for c in sobel mask')
 subplot(221)
 c=1;
 sobelX=double([-1 0 1; -c 0 c; -1 0 1]);
 sobelY=double([1 c 1; 0 0 0; -1 -c -1]);
-
 h=convolution(I,sobelX);
 v=convolution(I,sobelY);
 C=(abs(h)+abs(v));
@@ -92,7 +81,7 @@ subplot(224)
 c=10;
 sobelX=double([-1 0 1; -c 0 c; -1 0 1]);
 sobelY=double([1 c 1; 0 0 0; -1 -c -1]);
-startS=tic;
+startS=tic; %asked for part 6
 h=convolution(I,sobelX);
 v=convolution(I,sobelY);
 C=(abs(h)+abs(v));
@@ -101,11 +90,9 @@ imshow(C);
 title('c=10')
 
 
-%--------%
-% part 5 %
-%--------%
-% Use the Laplacian mask to edge detect same image, compare results
-%%
+
+%% Part 5
+% Use the Laplacian mask to edge detect same image, compare results.
 figure('name', 'laplacian mask')
 L=[0 1 0; 1 -4 1; 0 1 0];
 startL = tic;
@@ -114,25 +101,19 @@ laplacetime = toc(startL);
 imshow(im2uint8(laplacedI)), title('laplacian mask');
 rmpath ../commonFunctions
 
-%--------%
-% part 6 %
-%--------%
+
+%% Part 6
 % compare computation times
-%%
 sprintf('sobel approach: %s',sobeltime)
 sprintf('laplacian approach: %s', laplacetime)
 
 
+%% Problem 2
+% Edge detection in the frequency domain.
 
-%---------------------------------------------%
-% problem 2                                   %
-%---------------------------------------------%
-%--------%
-% part 1 %
-%--------%
+%% Part 1
 % Obtain the FFT of the horizontal and vertical Sobel masks.
 % You need to perform zero padding.
-%%
 addpath ../commonFunctions
 I2 = getIMG('Soyuz_TMA-19_spacecraft_departs_the_ISS.jpg');
 rmpath ../commonFunctions
@@ -143,7 +124,7 @@ figure('name','ffts of sobel masks');
 c=2;
 sobelX=double([-1 0 1; -c 0 c; -1 0 1]);
 sobelY=double([1 c 1; 0 0 0; -1 -c -1]);
-
+%work on making this more general..........
 sobelX1 = padarray(sobelX,[199,199]);
 sobelFFTX=fft2(sobelX1);
 %work on making this more general..........
@@ -153,11 +134,10 @@ sobelFFTY=fft2(sobelY1);
 subplot(121), imshow(sobelFFTX), title('sobelX fft');
 subplot(122), imshow(sobelFFTY), title('sobelY fft');
 
-%--------%
-% part 2 %
-%--------%
+
+%% Part 2
 % Perform filtering in the frequency domain using the horizontal Sobel mask
-%%
+
 figure('name', 'frequency horizontal filtering');
 horizF=fft2(I2).*sobelFFTX;
 horizS=ifft2(horizF);
@@ -165,11 +145,10 @@ dmin = min(min(abs(horizS)));
 dmax = max(max(abs(horizS)));
 subplot(121), imshow(horizF), title('horizontal sobel freq');
 subplot(122), imshow(ifftshift(horizS),[dmin dmax]), title('filtered img');
-%--------%
-% part 3 %
-%--------%
+
+
+%% Part 3
 % Perform filtering in the frequency domain using the vertical Sobel mask
-%%
 figure('name', 'frequency vertical filtering');
 vertF=fft2(I2).*sobelFFTY;
 vertS=ifft2(vertF);
@@ -177,43 +156,38 @@ dmin = min(min(abs(vertS)));
 dmax = max(max(abs(vertS)));
 subplot(121), imshow(vertF), title('horizontal sobel freq');
 subplot(122), imshow(ifftshift(vertS),[dmin dmax]), title('filtered img');
-%--------%
-% part 4 %
-%--------%
-% Obtain the FFT of the Laplacian mask. You need to perform zero padding
-%%
+
+
+%% Part 4
+% Obtain the FFT of the Laplacian mask. You need to perform zero padding.
 L1 = fft2(padarray(L,[199,199]));
 
-%--------%
-% part 5 %
-%--------%
-% Perform filtering in the frequency domain using Laplacian to detect the edges.
-%%
+
+%% Part 5
+% Perform filtering in the frequency domain using Laplacian to detect
+% the edges.
 figure('name','laplacian frequency filtering');
 lapF=fft2(I2).*L1;
 lapS=ifft2(lapF);
 dmin = min(min(abs(lapS)));
 dmax = max(max(abs(lapS)));
 imshow(ifftshift(lapS),[dmin dmax]), title('Laplacian filtered img freq');
-%--------%
-% part 6 %
-%--------%
+
+
+%% Part 6
 % Construct a 3 by 3 mask for the Laplacian of Gaussian (LoG) and then obtain 
 % its FFT. You can use fspecial for this question
-%%
-% addpath ../commonFunctions
-% gauss=gaussfilter(3, 1/2);
-% LoG=L*gauss;
-% LoGF=fft2(LoG);
-% rmpath ../commonFunctions
-LoG=fspecial('log',[401,401]);
+addpath ../commonFunctions
+gauss=gaussfilter(3, 1/2);
+LoG=L*gauss;
 LoGF=fft2(LoG);
+rmpath ../commonFunctions
+%LoG=fspecial('log',[401,401]);
+%LoGF=fft2(LoG);
 
-%--------%
-% part 7 %
-%--------%
+
+%% Parts 7 and 8
 % Perform filtering in the frequency domain using LoG to detect the edges
-%%
 figure('name','laplacian of gaussian freq filtering');
 I3=fft2(I2).*L1;
 lapS=ifft2(I3);
@@ -222,24 +196,13 @@ dmax = max(max(abs(lapS)));
 imshow(ifftshift(lapS),[dmin dmax]);
 title('Laplacian of gaussian filtered img freq');
 
-%--------%
-% part 8 %
-%--------%
-% Show and discuss your results.
-%%
 
 
-%---------------------------------------------%
-% problem 3                                   %
-%---------------------------------------------%
-%--------%
-% part 1 %
-%--------%
+%% Problem 3
 % The Harris detector was introduced in 1988 for corner detection.
 % An illustration is shown in figure 2. Apply the Harris
 % detector to an image of your choice. Pick an image with visible corners. 
 % Mark all corners on the image in a similar way to figure 2
-%%
 figure('name','corner detection');
 C=corner(I2); %to find corner points
 imshow(I2);hold on, plot(C(:,1),C(:,2),'ro')
