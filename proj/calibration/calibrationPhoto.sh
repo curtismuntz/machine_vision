638e6680568edb48bd0fa93e8172d09eb3c143ff
curtii <cmuntz@outlook.com>
2014-03-27 15:59:48 -0700

    stereo project started

---
+++ proj/calibration/calibrationPhoto.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#set the autofocus on the cameras to off
+v4l2-ctl -c focus_auto=0
+v4l2-ctl -d 1 -c focus_auto=0
+
+cd ~/Desktop/
+mkdir calibration/
+mkdir calibration/video0
+mkdir calibration/video1
+cd calibration
+
+# take 30 photos from each camera, pausing 3 seconds in between
+for (( i = 0; i < 30; i++ )); do
+	for (( j = 3; j >= 0; j-- )); do
+		sleep 1
+		echo "taking photo " + $i + " in " + $j + " seconds..."
+	done
+	ffmpeg -f v4l2 -i /dev/video0 "video0/video0_calib_%03d.jpg"
+	ffmpeg -f v4l2 -i /dev/video1 "video1/video1_calib1_%03d.jpg"
+done
+
+# not sure if i should re-enable autofocus
+#v4l2-ctl -c focus_auto=1
+#v4l2-ctl -d 1 -c focus_auto=1
---
+++ proj/ffmpegSolution.m
@@ -0,0 +1,84 @@
+%% Project.m
+% Most of the data is taken from this: % http://www.mathworks.com/help/matlab/examples/convert-between-image-sequences-and-video.html
+clear all, close all, clc;
+
+
+% Images and movies are too large and too many to store on this github page. They should be located on the desktop.
+workingDir = '/home/me/Desktop/stereo/';
+mkdir(workingDir);
+mkdir(workingDir,'images');
+mkdir(workingDir,'left');
+mkdir(workingDir,'right');
+vid = VideoReader('output.mp4');
+
+%convert each frame into an image
+for ii = 1:vid.NumberOfFrames
+    img = read(vid,ii);
+    %resolution decided to maximize frames per second
+	%crop each image to reclaim original frames
+    Left  = img([1:640],[1:480],:);
+	Right = img([641:1280],[1:480],:);
+    imwrite(Left,fullfile(workingDir,'images','left',sprintf('left%d.jpg',ii)));
+    imwrite(Right,fullfile(workingDir,'images','right',sprintf('right%d.jpg',ii)));
+    imwrite(img,fullfile(workingDir,'images',sprintf('img%d.jpg',ii)));
+end
+
+
+
+%% Convert left images into movie
+%now these are stored as individual image directories
+cd left
+imageNames = dir(fullfile(workingDir,'images','left','*.jpg'));
+imageNames = {imageNames.name}';
+
+%match filenames containing digits
+imageStrings = regexp([imageNames{:}],'(\d*)','match');
+imageNumbers = str2double(imageStrings);
+
+%sort frames from low to high
+[~,sortedIndices] = sort(imageNumbers);
+sortedImageNames = imageNames(sortedIndices);
+
+%frames are now sorted
+%make an output file:
+outputVideo = VideoWriter(fullfile(workingDir,'output.avi'));
+outputVideo.FrameRate = vid.FrameRate;
+open(outputVideo);
+
+%Loop through the image sequence, load each image, and then write it to the video.
+for ii = 1:length(sortedImageNames)
+    img = imread(fullfile(workingDir,'images',sortedImageNames{ii}));
+    writeVideo(outputVideo,img);
+end
+close(outputVideo);
+cd ..
+
+%% Convert right images into movie
+%now these are stored as individual image directories
+cd right
+imageNames = dir(fullfile(workingDir,'images','right','*.jpg'));
+imageNames = {imageNames.name}';
+
+%match filenames containing digits
+imageStrings = regexp([imageNames{:}],'(\d*)','match');
+imageNumbers = str2double(imageStrings);
+
+%sort frames from low to high
+[~,sortedIndices] = sort(imageNumbers);
+sortedImageNames = imageNames(sortedIndices);
+
+%frames are now sorted
+%make an output file:
+outputVideo = VideoWriter(fullfile(workingDir,'output.avi'));
+outputVideo.FrameRate = vid.FrameRate;
+open(outputVideo);
+cd ..
+%Loop through the image sequence, load each image, and then write it to the video.
+for ii = 1:length(sortedImageNames)
+    img = imread(fullfile(workingDir,'images',sortedImageNames{ii}));
+    writeVideo(outputVideo,img);
+end
+close(outputVideo);
+
+
+
